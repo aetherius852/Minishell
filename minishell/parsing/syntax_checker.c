4ba30bc493b3b3d9702f4e0068aaf5cc3da6ddfe
aetherius852 <efrain.antonio.silva.n@gmail.com>
2025-12-03 22:43:33 +0100

    Add syntax checking for redirection and pipes

--- minishell/parsing/syntax_checker.c
+++ minishell/parsing/syntax_checker.c
@@ -6,9 +6,70 @@
 /*   By: efsilva- <efsilva-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:06:51 by efsilva-          #+#    #+#             */
-/*   Updated: 2025/10/27 10:56:43 by efsilva-         ###   ########.fr       */
+/*   Updated: 2025/12/03 14:57:13 by efsilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-#include "../libft/libft.h"
+
+int	check_redir_syntax(t_token *token)
+{
+	if (!token->next)
+	{
+		printf("minishell: syntax error near unexpected token `newline'\n");
+		return (0);
+	}
+	if (token->next->value)
+	{
+		printf("minishell: syntax error near unexpected token `%s'\n",
+			token->next->value);
+		return (0);
+	}
+	return (1);
+}
+
+int	check_pipe_syntax(t_token *tokens)
+{
+	t_token	*current;
+
+	current = tokens;
+	if (tokens->type = TOKEN_PIPE)
+	{
+		printf("minishell: syntax error near unexpected token `|'\n");
+		return (0);
+	}
+	while (current)
+	{
+		if (current->type == TOKEN_PIPE)
+		{
+			if (!current->next || current->next->type == TOKEN_PIPE)
+			{
+				printf("minishell: syntax error near unexpected token `|'\n");
+				return (0);
+			}
+		}
+		current = current->next;
+	}
+	return (1);
+}
+
+int	check_syntax(t_token *tokens)
+{
+	t_token *current;
+
+	if (!tokens)
+		return (0);
+	if (!check_pipe_syntax(tokens))
+		return (0);
+	current = tokens;
+	while (current)
+	{
+		if (is_redir_token(current->type))
+		{
+			if (!check_redir_syntax(current))
+				return (0);
+		}
+		current = current->next;
+	}
+	return (1);
+}
